a43e67126715398eac94853d360c37c56dd1380a
mahehsma <maxi.hehl@gmail.com>
2023-11-09 15:05:39 +0100

    first commit

---
+++ myhistogram.R
@@ -0,0 +1,25 @@
+library(ggplot2)
+
+myhistogram = function(x, n=length(x)-1, min=base::min(x), max=base::max(x)) {
+
+  outside = x[x < min | x > max]
+  if(length(outside) > 0){
+    warning(paste('Zahl(en) außerhalb', paste(unlist(sort(outside)), collapse = ', ')))
+  }
+
+  filtered = x[x >= min & x <= max] # removes all values which are not in the interval defined by min and max
+  sorted = sort(filtered) # sorts vector ascending
+
+  interval_size = (max-min) / n
+  borders = seq(min, max, by=interval_size) # creates the list of borders of size n+1
+  counts = integer(n)
+
+  for(i in 1:n){
+    counts[i] = length(sorted[borders[i] <= sorted & sorted < borders[i+1]])
+  }
+  
+  return(list(
+    borders = borders,
+    counts = counts
+  ))
+}
