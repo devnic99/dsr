f19c132adef3068abb3312a2ce8e23d498444ccf
nicole <nicole.enzelsberger@gmx.at>
2024-01-09 19:17:40 +0100

    rename package and change tests

---
+++ testat-3/A3/frameinsight/R/visualize.R
@@ -0,0 +1,31 @@
+library(ggplot2)
+
+visualize <- function(df) {
+  numeric_columns <- sapply(df, is.numeric)
+
+  if (!any(numeric_columns)) {
+    warning("No numeric columns to visualize")
+    return(-1)
+  }
+
+  num_plots = 0
+  for (col_name in names(df)[numeric_columns]) {
+    print(col_name)
+    ggplot(df, aes(x = 1, y = df[[col_name]])) +
+      geom_boxplot() +
+      labs(title = col_name, y = col_name) +
+      theme_minimal()
+    num_plots = num_plots + 1
+  }
+  return(num_plots)
+}
+
+data = data.frame(
+  age = sample(18:60, n_rows, replace = TRUE),
+  income = rnorm(n_rows, mean = 50000, sd = 10000),
+  sex = sample(c("f", "m", "d"), n_rows, replace = TRUE),
+  height = rnorm(n_rows, mean = 175, sd = 10),
+  weight = rnorm(n_rows, mean = 70, sd = 5)
+)
+
+visualize(data)
--- testat-3/A3/frameinsight/tests/testthat.R
+++ testat-3/A3/frameinsight/tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(rrandom)
+library(frameinsight)
 
-test_check("rrandom")
+test_check("frameinsight")
